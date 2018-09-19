5eda02364c23609cb893cec5281a323100a8dea1
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-09-19 15:36:38 -0500

    finsihed Bob

---
+++ src/Bob.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Bob {
+
+    public static void main(String[] args) {
+        Scanner bobScanner = new Scanner(System.in);
+        boolean confirm = true;
+        while(confirm) {
+
+            System.out.println("Say something to Bob. Make sure you use punctuation.");
+            String speak = bobScanner.nextLine();
+            if(speak.endsWith("?")) System.out.println("Sure");
+            else if(speak.endsWith("!")) System.out.println("Whoa, chill out!");
+            else if(speak.contentEquals("")) System.out.println("Fine. Be that way!");
+            else System.out.println("Whatever.");
+        }
+
+
+
+
+
+    }
+}
