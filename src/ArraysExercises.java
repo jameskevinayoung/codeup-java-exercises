c57000f0ee42871e8b8e11df56d2ee6b16424ff2
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-09-24 14:32:17 -0500

    need help on the person section of the Array Exercises.java

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+
 
 public class ArraysExercises {
+
+
+
+
+
+
+
+    public static void main(String[] args){
+
+    //part 1
+
+       //part 1.a
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+        //you have to use the Arrays.toString() method and input the numbers array.
+        //else you won't be able to display any information.
+
+    //part 1.b
+
+        String[] person = {"person1", "person2", "person3"};
+
+        for(String perp:person){
+            System.out.println(perp);
+        }
+
+        Person me = new Person("james");
+
+
+
+    //part 1.c
+
+        //I'm confused on.
+
+
+
+}
+
+
 }
