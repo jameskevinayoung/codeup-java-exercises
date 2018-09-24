110e780425db68d7d02443a1dab8c8687a89d189
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-09-24 15:30:20 -0500

    finished ArraysExercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -3,9 +3,16 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
+    //1.c
 
+    //create
 
+    public static Person[] addPerson(Person[] people, Person newPerson){
+        Person[]newPeople = Arrays.copyOf(people, people.length+1);
+        newPeople[newPeople.length-1]= newPerson;
 
+        return newPeople;
+    }
 
 
 
@@ -16,28 +23,42 @@ public class ArraysExercises {
        //part 1.a
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
-        //you have to use the Arrays.toString() method and input the numbers array.
+        //comes out as [I@3545d6 meaning an array "[" of integers "I" at memory location "@3455d6";
+        //you have to use the Arrays.toString() method to format the array.
         //else you won't be able to display any information.
 
     //part 1.b
+        //use the person object generator from Person.java;
+        //Create an array that holds 3 Person objects;
+        //Assign a new instance of the Person class to each element.
+        //Iterate through the array and print out the name of each person in the array.
 
-        String[] person = {"person1", "person2", "person3"};
+        Person[] people = new Person[3];
+        people[0]= new Person ("James");
+        people[1]= new Person ("Kevin");
+        people[2] = new Person ("Alexander");
 
-        for(String perp:person){
-            System.out.println(perp);
+        for(Person person: people){
+            System.out.println(person.getName());}
+
+
+
+     // part 1.c
+        Person young = new Person("Young");
+        Person[] fourPeople = addPerson(people,young);
+        for (Person person:fourPeople){
+            System.out.println(person.getName());
         }
 
-        Person me = new Person("james");
+        }
 
 
 
-    //part 1.c
-
-        //I'm confused on.
 
 
 
-}
 
 
 }
+
+
