b5cc8b76bb4a455de8b2bb7666750fdfef0179c8
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-09-24 16:35:28 -0500

    update movies exercise

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,4 +1,31 @@
-package movies;
+package movies;//part 3.a
+import util.Input;
 
 public class Movie {
+
+
+    //part 3.b
+    //created private fields for the name and category;
+
+    private String name;
+    private String category;
+
+    //create getters for the movie name and category
+
+    public String getMovieName(String name){
+        String movieName = name;
+        return movieName;
+    }
+    public String getMovieCategory(String category){
+        String movieCategory = category;
+        return movieCategory;
+
+    }
+
+    public String getMovie(String name, String category){
+        this.name = name;
+        this.category = category;
+    }
+
+
 }
---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,23 @@
+package movies;
+
+public class MoviesApplication {
+//part 3.d
+    public static void main(String[] args){
+
+        //part 3.e
+        //give users a list of options
+
+        System.out.println("What would you like to do?");
+        System.out.println("0 - exit\n" +
+                "1 - view all movies\n" +
+                "2 - view movies in the animated category\n" +
+                "3 - view movies in the drama category\n" +
+                "4 - view movies in the horror category\n" +
+                "5 - view movies in the scifi category");
+        System.out.print("Enter your choice: ");
+
+
+
+
+    }
+}
