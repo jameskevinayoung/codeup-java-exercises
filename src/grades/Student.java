bb90423b11bb60cf63e956b1a91f1b5d64ae28ab
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-09-26 09:36:52 -0500

    begin of grades package

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,23 @@
 package grades;
 
 public class Student {
+
+
+    private String studentName;
+    private int grade;
+
+
+    public Student(String name, int grade) {
+        this.studentName = name;
+        this.grade = grade;
+    }
+
+//    // returns the student's name
+//    public String getName();
+//    // adds the given grade to the grades property
+//    public void addGrade(int grade);
+//    // returns the average of the students grades
+//    public double getGradeAverage();
+
+
 }
