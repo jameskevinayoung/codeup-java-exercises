c73b7f76394dcc979873829b90f2fe473268afcc
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-09-29 19:37:55 -0500

    added an array class

---
+++ src/Contacts/ContactsArray.java
@@ -0,0 +1,15 @@
+package Contacts;
+
+
+public class ContactsArray {
+    public static Contact[] findAll() {
+        return new Contact[]{
+                new Contact("Jakai Alexis","1234 Sudo Lane", "jakaiBAlexis@jumba.com","Personal",5152346789L),
+                new Contact("Rachael Karens", "227 Delaware Street", "rachaleReach@sitamimail.com", "Personal", 3435677890L),
+                new Contact("Michael Simmons", "7145 Delimeter Avenue", "mchaelSimmons@juma.com", "Business", 4453679213L),
+        };
+    }
+
+
+
+}
