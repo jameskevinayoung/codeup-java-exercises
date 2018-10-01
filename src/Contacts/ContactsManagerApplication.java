7a2e940d2ceffcd724f47fb3d6383306d430927e
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-10-01 09:01:47 -0500

    add minor changes

--- src/Contacts/ContactsManagerApplication.java
+++ src/Contacts/ContactsManagerApplication.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 public class ContactsManagerApplication {
 //    public static Scanner contactScanner = new Scanner (System.in);
 
-    private static void  displayContact(String filter){ //this is a method to display the movie options by category;
+    private static void  displayContact(String filter){ //this is a method to display the contact options by category;
         Contact[] all = ContactsArray.findAll();//create a variable for a movie array and assign the Movies array findAll method to it.
         if(filter.equals("View"))//if the user selects 1 then display all the movies
             for(Contact contact: all) System.out.println(contact.getContactName()+ ":"+contact.getContactAddress()+", "+
@@ -22,8 +22,7 @@ public class ContactsManagerApplication {
         else{
             for(Contact contact: all){
 //otherwise loop through each movie and if the movie category equals the user's selection print out the movies' name and category associated with the selection;
-                if(contact.getContactName().equals(filter))System.out.println(contact.getContactName()+ ":"+contact.getContactAddress()+", "+
-                        contact.getContactEmail()+", "+contact.getContactType()+", "+ contact.getContactPhoneNumber());
+                if(contact.getContactName().equals(filter))System.out.println(contact.getContactName());
             }
         }
     }
@@ -36,22 +35,22 @@ public class ContactsManagerApplication {
 
 
 
-//        Path ContactsDirectory = Paths.get("./contactsDirectory");
-//
-//        Path contacts = Paths.get("./contactsDirectory", "contacts.txt");
-//        List<String> contactLine = new ArrayList<>();
-//
-//        try{
-//            if(Files.notExists(ContactsDirectory)) {//was able to make the directory
-//                Files.createDirectory(ContactsDirectory);
-//            }if(Files.notExists(contacts)){
-//                contactLine = Files.readAllLines(contacts);
-//                for(String line:contactLine) System.out.println(line);
-//            }
-//        }catch(IOException e){
-//            System.out.println(e);
-//        }
-//        System.out.println(ContactsDirectory.toAbsolutePath());
+        Path ContactsDirectory = Paths.get("./contactsDirectory");
+
+        Path contacts = Paths.get("./contactsDirectory", "contacts.txt");
+        List<String> contactLine;
+
+        try{
+            if(Files.notExists(ContactsDirectory)) {//was able to make the directory
+                Files.createDirectory(ContactsDirectory);
+            }if(Files.notExists(contacts)){
+                contactLine = Files.readAllLines(contacts);
+                for(String line:contactLine) System.out.println(line);
+            }
+        }catch(IOException e){
+            System.out.println(e);
+        }
+        System.out.println(ContactsDirectory.toAbsolutePath());
 
 
         System.out.println("1. View contacts.\n" +
@@ -69,18 +68,18 @@ public class ContactsManagerApplication {
 
             System.out.println("Enter an option (1, 2, 3, 4 or 5):");
 
-            String userMenuInput = contactScanner.nextLine();
+            int userMenuInput = contactScanner.nextInt();
 
             switch(userMenuInput){
-                case("1"):
+                case 1:
                     displayContact("View contacts");break;
-                case("2"):
-                    displayContact("Add a new contact");break;
-                case("3"):
-                    displayContact("Search a contact by name");break;
-                case("4"):
+                case 2:
+//                    displayContact("Add a new contact");break;
+                case 3:
+                    displayContact("");break;
+                case 4:
                     displayContact("Delete an existing contact");break;
-                case("5"):
+                case 5:
                     return;
             }
 
