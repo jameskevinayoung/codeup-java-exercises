c730814f374e01c3355e5015b703b4d21da2515b
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-10-02 11:42:39 -0500

    contacts

--- src/Contacts/Contact.java
+++ src/Contacts/Contact.java
@@ -41,9 +41,44 @@ public class Contact {
     }
 
 
-    public Contact addContact(){
+    public Contact[] addContact(Contact[] contacts, String name, String address, String email, String contactType, long phoneNumber ){ //Step 1: we are taking in the original array and adding a contact to an array of contacts
+        //Step 4.b: we need to add in all of the parameters from the constructor inorder to build a new contact inside this method;
 
 
+
+        Contact[] updatedContact = new Contact[contacts.length +1];//Step 2: take the existing contact array and assigning it to a variable that is
+        //one space larger than the previous contacts array.
+
+        //Step 3: to keep the contents of the old array we have to loop through the array and add it to the updated contacts
+
+        for(int i = 0; i< contacts.length; i++) {
+
+            //Step 4.a: in the updatedContact array, the index of i is assigned the passed contact array value at the same index;
+            //this handles the old contacts;
+            updatedContact[i] = contacts[i];
+        }
+
+        // Step 5.a: after we loop through the entire old array we have to add another spot to the new array;
+        //we are going to have to use the same values in the constructor to form a new contact in this method;
+        //these items represent the parameters for building our new contact array
+        Contact newContact = new Contact(name, address, email,contactType, phoneNumber);
+
+        //Step 5.b: newContact needs to be at the last index of our array;
+        //to do that we take our updatedContact array and find the length of that array and subtract 1 to get the last index position;
+        /**
+         * example: the length of the original array is 3 (3 contacts in the array) but the index is 2 (0, 1, 2);
+         * we added an additional space to the updatedContact array (orginal array's length +1) which gives us an index of 3 (0, 1, 2, 3;
+         * now take the updated array's length (which is 4) and subtract by 1;
+         * this will give the index position 3 to our updatedContact;
+         * now we assign that position to the newContact.
+         */
+
+        updatedContact[updatedContact.length-1]=newContact;
+
+
+        //step 6: return the updatedContact array;
+
+        return updatedContact;
     }
 
 }
--- src/Contacts/ContactsManagerApplication.java
+++ src/Contacts/ContactsManagerApplication.java
@@ -1,5 +1,7 @@
 package Contacts;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,7 +16,7 @@ import java.util.Scanner;
 public class ContactsManagerApplication {
 //    public static Scanner contactScanner = new Scanner (System.in);
 
-    private static void  displayContact(String filter){ //this is a method to display the contact options by category;
+    private static Contact[]  displayContact(String filter){ //this is a method to display the contact options by category;
         Contact[] all = ContactsArray.findAll();//create a variable for a movie array and assign the Movies array findAll method to it.
         if(filter.equals("View"))//if the user selects 1 then display all the movies
             for(Contact contact: all) System.out.println(contact.getContactName()+ ":"+contact.getContactAddress()+", "+
@@ -25,34 +27,63 @@ public class ContactsManagerApplication {
                 if(contact.getContactName().equals(filter))System.out.println(contact.getContactName());
             }
         }
+        return all;
     }
 
 
     public static void main(String[] args) {
 
-//       Contact newContact = new Contact("Jakai Alexis","1234 Sudo Lane",
-//               "jakaiBAlexis@jumba.com","Personal",5152346789L);
+//       Contact newContact = new Contact("Belinda Miles","433d Tudor Lane",
+//               "BMiles@jumba.com","Business",1234567890);
+//
+//        System.out.println(newContact);
+//
+//        System.out.println(newContact.addContact(displayContact("View"), "Todd McFarris", "abidel drive", "tmc@jumba.com", "personal",
+//                1234567891));
 
+        //Create a contact.txt file; read its contents line by line; show all information
 
+        //Step 1: create a variable that holds contacts.text string;
         String filename = "contacts.txt";
 
-        Path ContactsDirectory = Paths.get("./contactsDirectory");
-        Path contactsFile = Paths.get("./contactsDirectory", filename);
+        //Step 2: find and retrieve the path of the directory and the file
 
+        Path ContactsDirectory = Paths.get("/Users/user/IdeaProjects/codeup-java-exercises/src/Contacts");
+        Path contactsFile = Paths.get("/Users/user/IdeaProjects/codeup-java-exercises/src/Contacts", filename);
 
-        List<String> contactLine;
+        //Step 3: now display the contents of the contacts.txt file;
+        //printing out Files.readAllLines(contactsFile) identified a potential IOException Error;
+        //use Try-Catch; if you don't get any information check your path, instead of using relative use the direct path instead;
 
         try{
-            if(Files.notExists(ContactsDirectory)) {//was able to make the directory
-                Files.createDirectory(ContactsDirectory);
-            }if(!Files.exists(contactsFile)){
-                Files.createFile(contactsFile);
-//                for(String line:contactLine) System.out.println(line);
-            }
+
+            //this is reading all of the lines in the text file
+            System.out.println(Files.readAllLines(contactsFile));
+
         }catch(IOException e){
-            System.out.println(e);
+
+            //this prints out the error message if there is one.
+            System.out.println(e.getMessage());
         }
-        System.out.println(ContactsDirectory.toAbsolutePath());
+
+
+//        List<String> contactLine;
+
+
+
+        //This is only needed if you need to create a directory and file
+//        try{
+//
+//            if(Files.notExists(ContactsDirectory)) {//was able to make the directory
+//                Files.createDirectory(ContactsDirectory);
+//            }if(!Files.exists(contactsFile)){
+//                Files.createFile(contactsFile);
+////                for(String line:contactLine) System.out.println(line);
+//            }
+//        }catch(IOException e){
+//            System.out.println(e);
+//        }
+//        System.out.println(ContactsDirectory.toAbsolutePath());
 
 
         System.out.println("1. View contacts.\n" +
@@ -74,7 +105,7 @@ public class ContactsManagerApplication {
 
             switch(userMenuInput){
                 case 1:
-//                    displayContact("View contacts");break;
+                    displayContact("View");break;
                 case 2:
 //                    displayContact("Add a new contact");break;
                 case 3:
